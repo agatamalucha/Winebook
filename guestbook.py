9b43d46c9e245121005085ce7ac02df4abd63276
agatamalucha <agata.malucha@gmail.com>
2019-05-17 21:40:37 +0100

    db

--- guestbook.py
+++ guestbook.py
@@ -1,16 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
-    digit =5*5
-    return str(digit)
+    return render_template('index.html')
 
-@app.route('/home', methods=['GET', 'POST'] )
+
+@app.route('/sign')
+def sign():
+    return render_template('sign.html')
+
+
+@app.route('/process', methods=['POST'])
+def process():
+    name = request.form['name']
+    comment = request.form['comment']
+    return render_template('index.html', name=name, comment=comment)
+
+
+@app.route('/home', methods=['GET', 'POST'])
 def home():
-    links=['https://www.youtube.com', 'https://www.bing.com','https://www.python.org','https://www.enkato.com' ]
+    links = ['https://www.youtube.com', 'https://www.bing.com', 'https://www.python.org', 'https://www.enkato.com']
     return render_template('example.html', myvar='flask example', links=links)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
